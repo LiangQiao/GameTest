bfec7b92f2337abff284659705bd6a22d91b2fe9
Nguyen Ngoc Trung <trunga0@gmail.com>
2012-03-04 19:57:17 +0800

    First commit GameLogic

---
+++ GameTest/GameTest/ETDGameEngine.h
@@ -0,0 +1,13 @@
+//
+//  ETDGameEngine.h
+//  GameTest
+//
+//  Created by Nguyen Ngoc Trung on 3/4/12.
+//  Copyright (c) 2012 University of Singapore. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ETDGameEngine : NSObject
+
+@end
---
+++ GameTest/GameTest/ETDGameEngine.m
@@ -0,0 +1,13 @@
+//
+//  ETDGameEngine.m
+//  GameTest
+//
+//  Created by Nguyen Ngoc Trung on 3/4/12.
+//  Copyright (c) 2012 University of Singapore. All rights reserved.
+//
+
+#import "ETDGameEngine.h"
+
+@implementation ETDGameEngine
+
+@end
